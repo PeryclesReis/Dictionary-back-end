777a67ffc44a22485e8fc18d1233b6b7dc068aca
Peryclesreis <perycles_reis@hotmail.com>
2022-08-17 19:33:24 -0300

    estruturacao do backEnd

---
+++ src/app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const router = require('../routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use('/api', router);
+
+// app.use((err, _req, res, _next) => {
+//   console.log(err.message);
+//   return res.status(500).json({ message: 'Deu ruim alguma coisa aqui!'});
+// });
+
+module.exports = app;
